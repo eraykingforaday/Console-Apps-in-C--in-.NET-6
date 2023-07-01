f9b2ed1430797ec99974aa544a21d0787a0d4895
Eray Yilmaz <erayyillmaz1@gmail.com>
2023-07-02 02:26:34 +0300

    first commit

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.WriteLine("Merhaba, C# Console Uygulamasına Hoş Geldiniz!");
+Console.Write("Adınızı Girin: ");
+string ad = Console.ReadLine();
+
+Console.WriteLine($"Merhaba, {ad}! Uygulamamıza hoş geldiniz.");
+Console.WriteLine("Çıkış yapmak için herhangi bir tuşa basın.");
+Console.ReadKey();
